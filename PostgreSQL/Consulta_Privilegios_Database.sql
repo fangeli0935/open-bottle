a81a58137a19df9636ca88a909dcff8263c8f255
Flaviano Angeli <42542455+fangeli0935@users.noreply.github.com>
2024-10-31 15:27:57 -0300

    Update Consulta_Privilegios_Database.sql

--- PostgreSQL/Consulta_Privilegios_Database.sql
+++ PostgreSQL/Consulta_Privilegios_Database.sql
@@ -17,3 +17,17 @@ FROM
     pg_database d
 JOIN 
     pg_roles r ON r.rolname = 'nome_da_role';
+
+/* Outros exemplos */
+SELECT 
+    r.rolname,
+    d.datname,
+    has_database_privilege(r.rolname, d.datname, 'CONNECT') AS can_connect,
+    has_database_privilege(r.rolname, d.datname, 'CREATE') AS can_create,
+    has_database_privilege(r.rolname, d.datname, 'TEMP') AS can_create_temp_tables
+FROM 
+    pg_roles r
+CROSS JOIN 
+    pg_database d
+WHERE 
+    r.rolname = 'NOME_DA_ROLE';
